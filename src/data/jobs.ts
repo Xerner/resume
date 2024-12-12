9f39bfba4cae3fa9db86574d372f3b1d5c2f3cbc
Kenneth Mead <kennethmead784@gmail.com>
2024-12-12 13:45:21 -0500

    added impact to each job

--- src/data/jobs.ts
+++ src/data/jobs.ts
@@ -9,17 +9,17 @@ export const JOBS: IJob[] = [
   {
     id: "D4179F3E-C254-4989-BA3E-EA9E11AF8909",
     occupationId: "7078AFDD-5BC8-4831-90B5-E427273D583E",
-    description: "Re-architected the applications legacy payment system"
+    description: "Re-architected the applications legacy payment system reducing technical debt by 50%"
   },
   {
     id: "3C5570AE-3A93-4EB8-AA92-CA9DA003C3B6",
     occupationId: "7078AFDD-5BC8-4831-90B5-E427273D583E",
-    description: "Created an SQL change tracker to automate migrations (database first approach)"
+    description: "Created an SQL change tracker to automate migrations saving 4 hours of work per dev per week"
   },
   {
     id: "AA19EB59-45F3-4E19-A5F5-96D6A54C0643",
     occupationId: "7078AFDD-5BC8-4831-90B5-E427273D583E",
-    description: "Architected the CI/CD pipeline for deploying with docker containers"
+    description: "Architected the CI/CD pipeline for deploying with docker containers that reduced deployment time by 90%"
   },
   {
     id: "CFBEF422-BA25-48C7-86FA-900E28BA8075",
@@ -34,12 +34,12 @@ export const JOBS: IJob[] = [
   {
     id: "E2113BAE-9B52-4E8D-883A-44E612EC3CD0",
     occupationId: "F0B86406-B303-4DE2-9F58-F384C54B9329",
-    description: "Architected, developed, and documented a validation and reporting application for J2534 (CAN) signals."
+    description: "Architected, developed, and documented a validation and reporting application for J2534 (CAN) signals. The application saved validation engineers 8 hours per week of testing"
   },
   {
     id: "F5E33D93-ACD1-470D-9C33-265FA084E5E0",
     occupationId: "7B9CD2A8-F374-4CF3-871A-EEEBDA326C14",
-    description: "Integrated and unit tested an existing platform-level performance testing framework into the teams codebase and CI/CD pipeline."
+    description: "Integrated and unit tested an existing platform-level performance testing framework into the teams codebase and CI/CD pipeline. Using the existing framework saved the team 6 months of development time"
   },
   {
     id: "10ABC141-C638-4D9C-BEE3-787685D19845",
@@ -49,7 +49,7 @@ export const JOBS: IJob[] = [
   {
     id: "9E830D7A-74AC-408E-84A7-6BDBA3E93F23",
     occupationId: "1B14C1B3-469B-4E97-A371-F359A6F96670",
-    description: "Automated processing test data for carbon canisters GWC/BWC tests"
+    description: "Automated processing test data for carbon canisters GWC/BWC tests saving test engineers 20 hours per week of data processing"
   },
   {
     id: "71636935-EE2C-4D8C-BE41-963EFC58C30D",
@@ -59,7 +59,7 @@ export const JOBS: IJob[] = [
   {
     id: "171F43AA-AF86-4B4F-BBE9-D95A776B19F1",
     occupationId: "1B14C1B3-469B-4E97-A371-F359A6F96670",
-    description: "Worked with management to automate, analyze, and report employee key performance indicator (KPI) data."
+    description: "Worked with management to automate, analyze, and report employee key performance indicator (KPI) data. This data went to prove the high performance of the team to upper management"
   },
   {
     id: "328F8A20-8A7C-4BB0-9E91-9160268B7A55",
